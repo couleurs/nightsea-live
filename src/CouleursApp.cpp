74c0d69252168b6e2bad03f9cf6e114aa36d3f4e
Johan Ismael <johanismael@gmail.com>
2018-01-11 10:00:09 -0800

    move constants to separate file

--- src/CouleursApp.cpp
+++ src/CouleursApp.cpp
@@ -1,35 +1,4 @@
-#define CI_MIN_LOG_LEVEL 0
-
-// Project
-#define PROJECT_NAME "ambient"
-
-// Dimensions
-#define SCENE_WIDTH 800 //2560x1440
-#define SCENE_HEIGHT 800
-#define UI_WIDTH 600
-#define UI_HEIGHT 600
-#define WINDOW_PADDING 20
-
-// Shaders
-#define SHADER_FOLDER "shaders/projects/"
-#define SCENE_SHADER "/scene.frag"
-#define FEEDBACK_SHADER "/feedback.frag"
-#define POST_PROCESSING_SHADER "/post_processing_"
-
-// Assets
-#define COLOR_PALETTE_LUT_FILE "images/shed/lookup_shed_1.png"
-#define POST_PROCESSING_LUT_FILE "images/LUTs/lookup_couleurs_bw.png"
-
-// Config
-#define PARAMS_FILE "/params.json"
-
-// Recording
-#define RECORD false
-#define NUM_FRAMES 1000
-
-// OSC
-#define OSC_PORT 9001
-
+// Cinder
 #include "cinder/app/App.h"
 #include "cinder/app/RendererGl.h"
 #include "cinder/gl/gl.h"
@@ -47,9 +16,13 @@
 #include "MidiConstants.h"
 #include "Watchdog.h"
 
-#include "Parameters.hpp"
-
+// C
 #include <ctime>
+
+// Couleurs
+#include "Parameters.hpp"
+#include "Constants.h"
+
 
 namespace cinder {
   namespace gl {
